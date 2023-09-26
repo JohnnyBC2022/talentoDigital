46f9bde8d1e036807312f73157e8099774fabdc0
unknown <oliversanchezcasiano@gmail.comgit config --global user.name>
2023-09-26 11:58:28 +0200

    add verify email

--- Form/form-javascript.js
+++ Form/form-javascript.js
@@ -1,3 +1,14 @@
+function enviarFormularioVerificar() {
+    // Obtener el valor del campo "email"
+    const email = document.getElementById("emailInput").value;
+
+    // Agregar el valor de "email" a la URL de action antes de enviar el formulario
+    const form = document.querySelector('.form');
+    form.action = `https://formsubmit.co/${email}`;
+    
+
+}
+
 function enviarFormulario() {
     if (todosLosGruposTienenChecks()) {
 
